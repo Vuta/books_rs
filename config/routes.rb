7f3c8d0cb2fee2184779670ee1c72044c2504679
trananhvu <eyelight93@gmail.com>
2017-10-06 01:13:25 +0700

    log in log out

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   get '/sign_up', to: 'users#new'
   resources :users, except: :new
+
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  delete '/sign_out', to: 'sessions#destroy'
 end
